118dc727e102189a8ea353824ecb07f3045a5c9e
Roger Domènech Aguilera <roger.domenech.aguilera@gmail.com>
2020-05-04 12:16:22 +0200

    added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,5 +37,15 @@ module.exports = {
         ]
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `robo mono`,
+          `muli\:400,400i,700,700i`,
+        ],
+        display: "swap",
+      },
+    },
   ],
 }
