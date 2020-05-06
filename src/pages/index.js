8056522c73c7b49b457e8bc293630628921a0b88
Roger Domènech Aguilera <roger.domenech.aguilera@gmail.com>
2020-05-06 17:47:55 +0200

    Added feature image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
-import { Container } from "../components"
+import { Container, FeatureImage } from "../components"
 
 const IndexPage = () => {
     return (
-        <Container></Container>
+        <Container>
+            <FeatureImage />
+        </Container>
     )
 }
 
