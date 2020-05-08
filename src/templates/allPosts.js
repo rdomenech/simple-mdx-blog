3a9da9d62934a57757f1d5eb87c02b8ee94a6d8c
Roger Domènech Aguilera <roger.domenech.aguilera@gmail.com>
2020-05-08 17:06:04 +0200

    Adding SEO component

--- src/templates/allPosts.js
+++ src/templates/allPosts.js
@@ -5,7 +5,8 @@ import {
     Content,
     ContentCard,
     FeatureImage,
-    Pagination
+    Pagination,
+    Seo
 } from "../components"
 import{ H1, P} from "../elements"
 
@@ -20,6 +21,7 @@ const allPosts = ({pageContext, data}) => {
 
     return (
         <Container>
+            <Seo />
             <FeatureImage />
             <Content>
                 <H1 textAlign="center" margin="0 0 1rem 0">
